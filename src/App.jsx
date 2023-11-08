39210a9be4b910571b77f40014a2140a66d08aab
ki-yud19 <dickyudhz@gmail.com>
2023-11-08 22:02:52 +0700

    "finishing project"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Cars from "./pages/Cars";
+import Navigation from "./components/LandingPage/Navbar";
+import Footer from "./components/LandingPage/Footer";
+import CarContextProvider from "./store/car-filter-context";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CarContextProvider>
+        <Router>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/cars" element={<Cars />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </CarContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
